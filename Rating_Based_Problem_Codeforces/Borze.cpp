5ac516f81b659c151c72b7b6b3a46ac0b52109c4
sayeed <122158413+sayeedsaadman@users.noreply.github.com>
2024-01-01 01:56:30 +0600

    Add files via upload

---
+++ Rating_Based_Problem_Codeforces/Borze.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    string s;
+    cin >> s;
+    for(int i=0;i<s.length();i++)
+    {
+        if(s[i]== '.')
+        {
+            cout<<"0";
+           // i++;
+        }
+        if(s[i]== '-')
+        {
+            if(s[i+1]== '.')
+            {
+                cout<<"1";
+                i++;
+            }
+            else
+            {
+                cout<<"2"; 
+                i++;
+            }
+        }
+    }
+}
