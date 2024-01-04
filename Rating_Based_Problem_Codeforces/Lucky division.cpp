30031b98db27938007879efbe263dadffb5c81ad
sayeed <122158413+sayeedsaadman@users.noreply.github.com>
2024-01-04 13:18:28 +0600

    Add files via upload

---
+++ Rating_Based_Problem_Codeforces/Lucky division.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+
+//shortcut
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define yes cout << "yes" << endl
+#define no cout << "no" << endl
+#define minus_one cout << -1 << endl
+#define nl cout<<endl
+
+// Loop 
+#define FOR0(i,n) for(int i = 0; i < n; i++) // 0 based indexing
+#define FOR1(i,n) for(int i = 1; i <=n; i++) // 1 based indexing
+
+// Reverse a number 
+ll reverse_num(ll n)
+{
+    ll tmp = n, ans = 0, r;
+    while (tmp)
+    {
+        r = tmp % 10;
+        ans = ans * 10 + r;
+        tmp /= 10;
+    }
+    return ans;
+}
+// find GCD
+ll gcd(ll num1, ll num2)
+{
+    ll a, b, r;
+    a = num1;
+    b = num2;
+    r = a % b;
+    while (r > 0)
+    {
+        a = b;
+        b = r;
+        r = a % b;
+    }
+    return b;
+}
+// find LCM
+ll lcm(ll num1, ll num2) 
+{
+     return (num1 * num2) / gcd(num1, num2); 
+}
+
+void solve()
+{
+    int lucky[12]={4,7,44,47,47,444,447,474,477,744,774,777}; // all among 1000
+	int num; // 78
+	cin>>num;
+	bool flag = false;
+	for(int i=0;i<12;i++){ 
+		if(num % lucky[i] == 0){
+			flag = true;
+			break;
+		}
+	}
+	if(flag){
+		YES;
+	} else {
+		NO;
+	}
+}
+int main()
+{
+    // ll t;
+    // cin>>t;
+    // while(t--)
+    // {
+        solve();
+    // }
+}
+
