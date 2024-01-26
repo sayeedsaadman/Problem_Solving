4866a95be680f76586a67e38af62b778a2a51776
sayeed <122158413+sayeedsaadman@users.noreply.github.com>
2024-01-27 01:23:27 +0600

    Add files via upload

---
+++ Rating_Based_Problem_Codeforces/sale.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+
+//shortcut
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define yes cout << "yes" << endl
+#define no cout << "no" << endl
+#define minus_one cout << -1 << endl
+#define nl cout<<endl
+
+// Loop 
+#define FOR0(i,n) for(int i = 0; i < n; i++) // 0 based indexing
+#define FOR1(i,n) for(int i = 1; i <=n; i++) // 1 based indexing
+
+// void solve()
+// {
+   
+
+
+// }
+int main()
+{
+    // ll t=1;
+    // cin>>t;
+    // while(t--)
+    // {
+    //      solve();
+    // }
+   // solve();
+    int n,m;
+    cin>>n>>m;
+    int arr[n];
+    int sum =0 ;
+    int k = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i]; 
+    }
+    sort(arr,arr+n);
+    for (int i = 0; i < n; i++)
+    {
+        if(arr[i]<0){
+            sum+=arr[i];
+            k++;
+        }
+        if(k == m){
+            break;
+        } 
+    }
+     
+        
+    
+    cout<<abs(sum)<<endl;
+    
+
+
+
+}
+
