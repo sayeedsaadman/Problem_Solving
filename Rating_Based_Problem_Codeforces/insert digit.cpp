6ab3a9f3ba970a1f5ace6dade0875a56ab2921a6
sayeed <122158413+sayeedsaadman@users.noreply.github.com>
2024-01-27 01:22:56 +0600

    Add files via upload

---
+++ Rating_Based_Problem_Codeforces/insert digit.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double dbl;
+
+//shortcut
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+#define yes cout << "yes" << endl
+#define no cout << "no" << endl
+#define minus_one cout << -1 << endl
+#define nl cout<<endl
+
+// Loop 
+#define FOR0(i,n) for(int i = 0; i < n; i++) // 0 based indexing
+#define FOR1(i,n) for(int i = 1; i <=n; i++) // 1 based indexing
+
+void solve()
+{
+    int n,insert;
+    cin>>n>>insert;
+    string arr;
+    cin>>arr;
+    bool f = true;
+    for (int i = 0; i < n; i++)
+    {
+        if(f){
+            if((arr[i] - '0')<insert){
+                cout<<insert<<arr[i];
+                f=false;
+            }
+            else cout<<arr[i];
+        }
+        else cout<<arr[i];
+    }
+    if(f)cout<<insert;
+    nl;
+
+}
+int main()
+{
+    ll t=1;
+    cin>>t;
+    while(t--)
+    {
+         solve();
+    }
+    
+
+
+
+}
+
