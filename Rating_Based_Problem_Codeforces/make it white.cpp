e36849353d15ac4eb9ca7a61e48f03bde43a5b71
sayeed <122158413+sayeedsaadman@users.noreply.github.com>
2024-02-10 21:55:49 +0600

    Add files via upload

--- Rating_Based_Problem_Codeforces/make it white.cpp
+++ Rating_Based_Problem_Codeforces/make it white.cpp
@@ -19,22 +19,50 @@ typedef double dbl;
 #define FOR1(i,n) for(int i = 1; i <=n; i++) // 1 based indexing
 
 void solve() {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-    int cnt = 0;
-    int start = -1;
-    FOR0(i,n){
-        if (s[i] == 'B'){
-            if (start == -1){
-                start = i; 
-            }
-            cnt = i - start + 1; 
-        }
-    }
+    // int n;
+    // cin >> n;
+    // string s;
+    // cin >> s;
+    // int cnt = 0;
+    // int start = -1;
+    // FOR0(i,n){
+    //     if (s[i] == 'B'){
+    //         if (start == -1){
+    //             start = i; 
+    //         }
+    //         cnt = i - start + 1; 
+    //     }
+    // }
 
-    cout << cnt << endl;
+    // cout << cnt << endl;
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int end = -1; int beg = -1;int cnt =0; int cnt2= 0;
+    for (int i = n; i > 0; i--)
+    {
+        if(s[i] == 'B'){
+            end = i; //5 //2
+            break;
+        }
+        else end =0;
+    }   
+    for (int i = 0; i < n; i++)
+    {
+        if(s[i] == 'B') {    
+            beg = i; //1
+            break;  
+        } 
+        else beg =0;
+    }
+    
+    cout<<(end-beg) +1 <<endl;
+    
+    
+    
+    
+
 }
 
 int main() {
